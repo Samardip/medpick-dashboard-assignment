68165664769a8c15924865951991b2b0332a7146
Samardip Mandal <samardipmandal.kgp@gmail.com>
2024-11-17 00:03:57 +0530

    changed overflow for dashboard

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div className='w-1/6 px-8 py-[30px] h-[100vh]'>
         <Sidebar />
       </div>
-      <div className='w-5/6 bg-gray-100 h-[100vh] overflow-y-scroll'>
+      <div className='w-5/6 bg-gray-100 h-[100vh] overflow-y-scroll overflow-x-hidden'>
         <Navbar />
         <div className='flex'>
           <div className='flex flex-col  h-[100%] w-[65%] '>
