3b08e07b32b20e444d5aae44fb2ef7d5076f8c05
Samardip Mandal <samardipmandal.kgp@gmail.com>
2024-11-16 23:22:41 +0530

    added dashboard ui

--- src/component/barChart/barChart.jsx
+++ src/component/barChart/barChart.jsx
@@ -4,23 +4,27 @@ import { BarChart } from '@mui/x-charts/BarChart';
 export default function ChartsOverviewDemo() {
     let dark = false;
     return (
+        <div className="py-4 px-6 bg-white rounded-[8px] shadow-sm">
             <BarChart
                 series={[
                     {
-                        data: [35, 44, 24, 34, 20, 30, 10], 
+                        data: [35, 44, 24, 34, 20, 30, 10],
                         color: 'rgba(0, 149, 255, 1)',
                         barStyle: {
                             borderTopLeftRadius: '10px', // Top left corner radius
                             borderTopRightRadius: '10px', // Top right corner radius
                         },
+                        barWidth: 10, // Adjust bar width for this series
+                        strokeWidth: 10,
                     },
                     {
-                        data: [51, 6, 27, 30, 15, 24, 29], 
+                        data: [51, 6, 49, 30, 15, 24, 29],
                         color: 'rgba(243, 148, 30, 1)',
                         barStyle: {
                             borderTopLeftRadius: '10px', // Top left corner radius
                             borderTopRightRadius: '10px', // Top right corner radius
-                        }
+                        },
+                        barWidth: 10, // Adjust bar width for this series
                     }
                 ]}
                 sx={{
@@ -66,11 +70,14 @@ export default function ChartsOverviewDemo() {
                             'Sunday',
                         ],
                         scaleType: 'band',
+                        categoryGapRatio: 0.7,
+                        barGapRatio: 0.5
                     },
                 ]}
-                margin={{ top: 10, bottom: 50, left: 40, right: 10 }} // Increased bottom margin to 50
+                margin={{ top: 10, bottom: 50, left: 40, right: 10 }}
                 barGap={5} // Adjusts gap between bars within a group
                 barCategoryGap={50} // Adjusts gap between categories (x-axis labels)
             />
+        </div>
     );
 }
